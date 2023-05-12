5c4b592eabdfdd886d23615a678435b8b619687f
Ivan Albert <ivanalbert2911@gmail.com>
2023-05-12 18:58:11 +0700

    change color, remove refresh from review

--- resources/views/vendorLogin.blade.php
+++ resources/views/vendorLogin.blade.php
@@ -11,7 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     <body>
-        <section class="vh-100" style="background: linear-gradient(257deg, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 100%)">
+        <section class="vh-100" style="background: linear-gradient(257deg, rgb(78, 179, 11) 0%, rgb(0, 255, 224) 100%)">
             <div class="container py-5 h-100">
               <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
