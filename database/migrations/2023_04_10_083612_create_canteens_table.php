b066dcef17b8df5aac5d508e6b8a8fb82b6359cb
VannesL <86096572+VannesL@users.noreply.github.com>
2023-05-26 15:43:52 +0700

    Update 2023_04_10_083612_create_canteens_table.php

--- database/migrations/2023_04_10_083612_create_canteens_table.php
+++ database/migrations/2023_04_10_083612_create_canteens_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('address');
             $table->integer('favorites');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
