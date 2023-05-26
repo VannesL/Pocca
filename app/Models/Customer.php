a3fa5c9da196a2362ddf14f3db4ee51ba201fef1
Garvyn <85630396+GarvynTanaka@users.noreply.github.com>
2023-05-26 15:26:13 +0700

    Soft delete

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     protected $guarded = ['id'];
     protected $fillable = ['email', 'password', 'name', 'phone_number', 'dob'];
