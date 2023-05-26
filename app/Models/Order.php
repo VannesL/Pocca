d5f4c28eb5ebd8ca8fad8561f3657406b8b6283c
Garvyn <85630396+GarvynTanaka@users.noreply.github.com>
2023-05-26 17:51:13 +0700

    [ADD] WithTrashed on OrderModel

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
 
     public function customer()
     {
-        return $this->belongsTo(Customer::class);
+        return $this->belongsTo(Customer::class)->withTrashed();
     }
 
     public function vendor()
